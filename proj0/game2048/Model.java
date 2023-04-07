4a0d02bb8d95b9dec450b1c9977dacec3fc08a41
yangjibing <28065317@qq.com>
2023-04-07 15:34:46 +0800

    Done with project 0

--- proj0/game2048/Model.java
+++ proj0/game2048/Model.java
@@ -5,7 +5,7 @@ import java.util.Observable;
 
 
 /** The state of a game of 2048.
- *  @author TODO: YOUR NAME HERE
+ *  @author Jibing Yang
  */
 public class Model extends Observable {
     /** Current contents of the board. */
@@ -105,20 +105,82 @@ public class Model extends Observable {
      * 3. When three adjacent tiles in the direction of motion have the same
      *    value, then the leading two tiles in the direction of motion merge,
      *    and the trailing tile does not.
-     * */
+     *
+     *    The method works as follows: first use the board.setViewingPerspective(side)
+     *    method to set the board as if it's facing north, then run the moveCol
+     *    method on each column, finally, set the board in its initial perspective
+     *    using board.setViewingPerspective(Side.NORTH).
+     */
     public boolean tilt(Side side) {
         boolean changed;
         changed = false;
 
-        // TODO: Modify this.board (and perhaps this.score) to account
-        // for the tilt to the Side SIDE. If the board changed, set the
-        // changed local variable to true.
+        board.setViewingPerspective(side);
+        int size = board.size();
+        for (int col = 0; col < size; col++) {
+            changed = moveCol(changed, size, col);
+        }
+        board.setViewingPerspective(Side.NORTH);
 
         checkGameOver();
         if (changed) {
             setChanged();
         }
         return changed;
+    }
+
+    /** This method does the majority of work in tilt.
+     *  On any given col, from up to bottom it tracks
+     *  down each tile and moves them one by one to
+     *  their supposed position. I use a boolean canMerge
+     *  to indicate whether the current tile can merge
+     *  with the last one. With that known, I can set
+     *  the destination of this tile to be the position
+     *  of the last tile or one row under it.
+     *
+     *  The instance variable score is updated during the
+     *  run of this method, if merges happen.
+     *
+     *  Returns true if there's any change to the board.
+     */
+    private boolean moveCol(boolean changed, int size, int col) {
+        int dest = size - 1;
+        int row = findTileInRow(board, col, dest);
+        boolean canMerge = false;
+        while (row >= 0) {
+            Tile t = board.tile(col, row);
+            int thisValue = t.value();
+            if (canMerge && board.tile(col, dest + 1).value() == thisValue) {
+                dest++;
+            }
+
+            boolean mergeHappens = false;
+            if (dest != row) {
+                mergeHappens = board.move(col, dest, t);
+                changed = true;
+                if (mergeHappens) {
+                    score += thisValue * 2;
+                }
+            }
+            canMerge = !mergeHappens;
+            dest--;
+            row = findTileInRow(board, col, dest);
+        }
+        return changed;
+    }
+
+    /** Helper function to find the next tile, starting from position
+     *  "row" and counting down to 0. Returns -1 if the method doesn't
+     *  find one.
+     */
+    private static int findTileInRow(Board b, int col, int row) {
+        while (row >= 0) {
+            if (b.tile(col, row) != null) {
+                return row;
+            }
+            row--;
+        }
+        return row;
     }
 
     /** Checks if the game is over and sets the gameOver variable
@@ -135,9 +197,16 @@ public class Model extends Observable {
 
     /** Returns true if at least one space on the Board is empty.
      *  Empty spaces are stored as null.
-     * */
+     */
     public static boolean emptySpaceExists(Board b) {
-        // TODO: Fill in this function.
+        int size = b.size();
+        for (int col = 0; col < size; col++) {
+            for (int row = 0; row < size; row++) {
+                if (b.tile(col, row) == null) {
+                    return true;
+                }
+            }
+        }
         return false;
     }
 
@@ -147,7 +216,17 @@ public class Model extends Observable {
      * given a Tile object t, we get its value with t.value().
      */
     public static boolean maxTileExists(Board b) {
-        // TODO: Fill in this function.
+        int size = b.size();
+        for (int col = 0; col < size; col++) {
+            for (int row = 0; row < size; row++) {
+                Tile t = b.tile(col, row);
+                /** Need to check the tile for non-empty value first.
+                 *  Otherwise will get an error accessing its value. */
+                if (t != null && t.value() == MAX_PIECE) {
+                    return true;
+                }
+            }
+        }
         return false;
     }
 
@@ -156,12 +235,65 @@ public class Model extends Observable {
      * There are two ways that there can be valid moves:
      * 1. There is at least one empty space on the board.
      * 2. There are two adjacent tiles with the same value.
+     *
+     * Use the emptySpaceExists(b) method above to check the
+     * first condition. Write a new method mergeableTilesExist(b)
+     * to check the second condition.
      */
     public static boolean atLeastOneMoveExists(Board b) {
-        // TODO: Fill in this function.
+        return emptySpaceExists(b) || mergeableTilesExist(b);
+    }
+
+    /**
+     * Returns true if any two adjacent tiles with the same
+     * value exists. Note that mergeableTilesExist(b) assumes
+     * all the tiles are non-empty.
+     *
+     * To make this algorithm more efficient, do checks on
+     * every other tile, instead of every tile. Because the
+     * check cases for the leftover ones are already covered
+     * by the ones adjacent to them. In this case, the chosen
+     * tile has a property of col % 2 == row % 2.
+     */
+    private static boolean mergeableTilesExist(Board b) {
+        int size = b.size();
+        for (int col = 0; col < size; col++) {
+            // Filter for (col % 2 == row % 2).
+            int filter = col % 2;
+            for (int row = 0; row < size; row++) {
+                if (row % 2 == filter && adjacentTilesIdentical(b, col, row)) {
+                    return true;
+                }
+            }
+        }
         return false;
     }
 
+    /**
+     * Returns true if any of the four tiles adjacent to this
+     * one has the same value.
+     */
+    private static boolean adjacentTilesIdentical(Board b, int col, int row) {
+        return inboundAndEqual(b, col, row, col - 1, row)
+                || inboundAndEqual(b, col, row, col + 1, row)
+                || inboundAndEqual(b, col, row, col, row - 1)
+                || inboundAndEqual(b, col, row, col, row + 1);
+    }
+
+    /**
+     * Returns true if the other tile is inbound and
+     * has the same value.
+     */
+    private static boolean inboundAndEqual(Board b, int col, int row, int otherCol, int otherRow) {
+        int size = b.size();
+        if (otherCol < 0 || otherCol >= size) {
+            return false;
+        }
+        if (otherRow < 0 || otherRow >= size) {
+            return false;
+        }
+        return b.tile(col, row).value() == b.tile(otherCol, otherRow).value();
+    }
 
     @Override
      /** Returns the model as a string, used for debugging. */
